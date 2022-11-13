40732835bdc3b58b6c0dbdfc5708a52af7ac64f4
AbdEl-Rahman-Fawzy <102333002+AbdEl-Rahman-Fawzy@users.noreply.github.com>
2022-11-13 19:32:45 +0200

    Update BankAccount.h

--- BankAccount.h
+++ BankAccount.h
@@ -4,19 +4,39 @@
 #include "Client.h"
 
 using namespace std;
-
+class Client ;
 class BankAccount
 {
-    private:
-        string ID;
-        double balance ;
-        Client * ptr ;
-    public:
-        BankAccount(string id , double balance);
-        void set_ptr(  Client *) ;
-        double get_balance();
-        virtual bool withdraw(double x) ;
-        virtual void deposite(double x) ;
+protected:
+    string ID;
+    double balance{} ;
+    Client * ptr{} ;
+public:
+    BankAccount(string id , double balance);
+
+    virtual void set_ptr(  Client *) ;
+
+    virtual double get_balance();
+
+    __attribute__((unused)) virtual bool withdraw       (double x) ;
+    virtual void deposite       (double x) ;
+    virtual void set_id         (string s)  ;
+    virtual void set_balance    (double s)  ;
+    BankAccount()= default;
+    ~BankAccount()= default;
 };
 
+
+class SavingsBankAccount: public BankAccount{
+public:
+    __attribute__((unused)) SavingsBankAccount(string id , double balance);
+    void    set_ptr         (Client *) override ;
+    double  get_balance     ()         override ;
+    bool    withdraw        (double x) override ;
+    void    deposite        (double x) override ;
+    void    set_id          (string s) override ;
+    void    set_balance     (double s) override ;
+
+
+};
 #endif // BANKACCOUNT_H
