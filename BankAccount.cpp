3a83a913496c7f0f03b607a41f6cd52dd55aa952
AbdEl-Rahman-Fawzy <102333002+AbdEl-Rahman-Fawzy@users.noreply.github.com>
2022-11-13 19:32:15 +0200

    Update BankAccount.cpp

--- BankAccount.cpp
+++ BankAccount.cpp
@@ -1,10 +1,12 @@
 #include "BankAccount.h"
 
+#include <utility>
+
 BankAccount::BankAccount(string id , double balance)
 {
-    ID =id;
+    ID =std::move(id);
     this->balance = balance ;
-    ptr= NULL;
+    ptr= nullptr;
 }
 
 void BankAccount::set_ptr(  Client *a) {
@@ -18,11 +20,65 @@ double BankAccount::get_balance(){
 bool BankAccount::withdraw(double x){
     if (balance>=x){
         balance-=x;
-        return 1;
+        return true;
     }
-    return 0;
+    return false;
 }
 
 void BankAccount::deposite(double x){
     balance +=x;
+}
+
+void BankAccount::set_id(string s) {
+    ID = std::move(s);
+}
+
+void BankAccount::set_balance(double s) {
+    balance = s;
+}
+SavingsBankAccount::SavingsBankAccount(string id, double balance)
+{
+    ptr= nullptr;
+    if(balance<100){
+        cout<<"At least u must save more than 100 $";
+        return;
+    }
+    else{
+        ID = std::move(id);
+        this->balance = balance ;
+
+    }
+}
+
+void SavingsBankAccount::set_ptr(  Client *a) {
+    ptr = a;
+}
+
+double SavingsBankAccount::get_balance(){
+    return balance;
+}
+
+
+bool SavingsBankAccount::withdraw(double x){
+    if (balance-1000>=x){
+        balance -=  x;
+        return true;
+    }
+    return false;
+}
+
+void SavingsBankAccount::deposite(double x){
+    if(x>=100)
+        balance +=x;
+    else{
+        cout<<"u must at least seposite more than 100 $_$\n";
+    }
+}
+
+void SavingsBankAccount::set_id(string s) {
+    BankAccount::set_id(s);
+}
+
+void SavingsBankAccount::set_balance(double s) {
+    BankAccount::set_balance(s);
 }
