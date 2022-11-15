fa93a5310afae590b746f96c903d855aa3876401
AbdEl-Rahman-Fawzy <102333002+AbdEl-Rahman-Fawzy@users.noreply.github.com>
2022-11-15 15:03:39 +0200

    Update BankingApp.cpp

--- BankingApp.cpp
+++ BankingApp.cpp
@@ -1,60 +1,188 @@
 #include "BankingApp.h"
 
 void BankingApp::menu(){
-    cout<<"welcome \n";
-    cout<<"to add acc enter 1\nif you want to login enter 2\n";
-    int x;
-    cin>>x;
-    if (x == 1){
+    ff:
+    cout<<"Welcome to FCAI Banking Application \n"
+          "1. Create a New Account \n"
+          "2. List Clients and Accounts \n"
+          "3. Withdraw Money \n"
+          "4. Deposit Money\n "
+          "5. exit\n";
+    cout<<"Please Enter Choice>>>>>>";
+    int choice;cin>>choice;
+    if(choice>5 || choice<1){
+        cout<<"\n invalid choice choose another one\n";
+        goto ff;
+    }
+    if (choice == 1){
         addAccount();
     }
-    if (x==2){
-        string id;
-        cin>>id;
-        cout<<"if you want to deposite enter 1\n";
-        cin>>x;
-        if (x==1){
-            cout<<"enter the amount of money\n";
-            double money;
-            cin>>money ;
-            accounts[stoi(id)-1].deposite(money);
-            cout<<(*clients[stoi(id)-1].ptr).get_balance()<<endl;
+
+    if (choice == 2){
+        for(int i = 0 ; i < clients.size() ; i ++){
+
+            cout<<"\n----------"<<clients[i].get_name()<<"--------"<<endl;
+            cout<<"Address:"<<clients[i].get_address()<<endl;
+            cout<<"Phone:"<<clients[i].get_phone()<<endl;
+            cout<<"ID :"<<i+1<<endl;
+            if(clients[i].pointer== nullptr){
+                cout<<"Balance :"<<clients[i].ptr->get_balance()<<endl;
+            }
+            else{
+                cout<<"Balance :"<<clients[i].pointer->get_balance()<<endl;
+            }
+        }
+    }
+
+
+    if (choice==3){
+        gg:
+        cout<<"\nPlease Enter Account ID =========>";
+        int i;      cin>>i;
+        i--;
+        if(i>clients.size()){
+            cout<<"invalid ID"<<endl;
+            int y ;cin>>y;
+            cout<<"\n If u want to back to the menu press 5";
+            if(y == 5 ) {
+                this->menu();
+                return;
+            }
+            else
+                goto gg;
+        }
+        cout<<"Account ID:"<<i+1;
+        if(clients[i].pointer== nullptr){
+            cout<<"\nAccount Type: Basic\n";
+            cout<<"Balance :"<<clients[i].ptr->get_balance()<<endl;
+        }
+        else{
+            cout<<"Account Type: Saving\n";
+            cout<<"Balance :"<<clients[i].pointer->get_balance()<<endl;
+        }
+        start:
+        cout<<"Please Enter The Amount to Withdraw =========>";
+        double x ; cin>>x;
+
+        if(clients[i].pointer== nullptr){
+            if(!clients[i].ptr->withdraw(x)){
+                cout<<"this is to much u dont have enough balance\n";
+                goto start;
+            }
+        }
+        else{
+            if(!clients[i].pointer->withdraw(x)){
+                cout<<"this is to much u dont have enough balance\n";
+                goto start;
+            }
+        }
+
+        cout<<"\nThanks da yo .";
+        cout<<"\nNew balance :";
+        if(clients[i].pointer== nullptr){
+            cout<<clients[i].ptr->get_balance()<<endl;
+        }
+        else{
+            cout<<clients[i].pointer->get_balance()<<endl;
         }
 
     }
-    if (x == 3){
-        for(auto i:clients){
-            cout<<i.get_name()<<endl;
+    if(choice==4){
+        cout<<"\nPlease Enter Account ID =========>";
+        int i;      cin>>i;
+        cout<<"Account ID:"<<i;
+        if(clients[i].pointer== nullptr){
+            cout<<"\nAccount Type: Basic\n";
+            cout<<"Balance :"<<clients[i].ptr->get_balance()<<endl;
         }
+        else{
+            cout<<"Account Type: Saving\n";
+            cout<<"Balance :"<<clients[i].pointer->get_balance()<<endl;
+        }
+        cout<<"Please Enter The Amount to deposit =========>";
+        double x ; cin>>x;
+        if(clients[i].pointer== nullptr){
+            clients[i].ptr->deposit(x);
+        }
+        else{
+            clients[i].pointer->deposit(x);
+        }
+        cout<<"\nThanks da yo .";
+        cout<<"\nNew balance :";
+        if(clients[i].pointer== nullptr){
+            clients[i].ptr->get_balance();
+        }
+        else{
+            clients[i].pointer->get_balance();
+        }
+    }
+    if(choice == 5){
+        cout<<"\nThanks for using our Bank .";
+        return;
     }
 }
 
 
 void BankingApp::addAccount(){
-    cout<<"if u want it saving account enter 2 otherwise enter 1";
-    int k ;     cin>>k;
-    cout<<"Enter the Account data\n";
     string name ,phone,address;
-    double balance ;
-    int ID ;
-    cout<<"Name >> Phone number >> Address >> Balance";
-    cin>>name>>phone>> address>>balance ;
+    cout<<"Please Enter Client User Name =========>";
+    cin.clear();
+//    cin.ignore();
+    cin.sync();
+    getline(cin,name);
+    cout<<"\nPlease Enter Client Address =======>";
+    cin.clear();
+//    cin.ignore();
+    cin.sync();
+    getline(cin,address);
+    cout<<"\nPlease Enter Client Phone =======>";
+    cin.clear();
+//    cin.ignore();
+    cin.sync();
+    getline(cin,phone);
+
+
+    cout<<"\nWhat Type of Account Do You Like? (1) Basic (2) Saving  Type 1 or 2 =========>";
+    int choice ;    cin>>choice;
+
+    while(choice >2 || choice<1){
+        cout<<"Please renter available choice 1 or 2 \n";
+        cin>>choice;
+    }
+    cout<<"\nPlease Enter the Starting Balance =========>";
+    double balance; cin>>balance;
+
+
+    unsigned long long ID ;
     ID = clients.size()+1;
     string id = to_string(ID) ;
-    cout<<"YOUR ID IS "<<id<<endl;
-    Client c(name,id,phone,address );
+    cout<<"Your id is :"<<id;
+    cout<<endl;
+    if(choice==1){
+        Client c(name, id, phone, address );
+        BankAccount b(id,balance);
+        //client.insert({name,c});
+        clients.push_back(c) ;
+        accounts.push_back(b) ;
+        c.set_ptr( & accounts.back() ) ;//last account made
+        clients.back() = c ;
+        b.set_ptr(& clients.back()) ;
+        accounts.back() = b ;
+        cout<<"\nAn account was created with ID and Starting Balance "<<balance;
+    }
+    if(choice==2){
+        Client c(name,id,phone,address );
+        SavingsBankAccount b(id,balance);
+        //client.insert({name,c});
+        clients.push_back(c) ;
+        Savings.push_back(b) ;
+        c.set_pointer( & Savings.back() ) ;//last account made
+        clients.back() = c ;
+        b.set_ptr(& clients.back()) ;
+        Savings.back() = b ;
+        cout<<"\nAn account was created with ID FCAI-001 and Starting Balance "<<balance;
+    }
 
-    BankAccount b(id,balance);
-
-
-
-    clients.push_back(c) ;
-    accounts.push_back(b) ;
-    c.set_ptr(&accounts.back()) ;
-    clients.back() = c ;
-    b.set_ptr(& clients.back()) ;
-    accounts.back() = b ;
 
 }
-
 
