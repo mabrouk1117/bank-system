5684451cfd9fbfa4f5e6508268fbbf40d77b85bc
AbdEl-Rahman-Fawzy <102333002+AbdEl-Rahman-Fawzy@users.noreply.github.com>
2022-11-15 15:04:33 +0200

    Update Client.h

--- Client.h
+++ Client.h
@@ -1,10 +1,10 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 #include<bits/stdc++.h>
-
 using namespace std;
 
 class BankAccount ;
+class SavingsBankAccount ;
 
 class Client
 {
@@ -13,13 +13,20 @@ private:
     string ID;
     string phone;
     string address;
-public:
-    BankAccount *ptr ;
-    Client(string n , string id , string phone , string address ) ;
-    void set_ptr(  BankAccount *) ;
+    int type{};
 
+public:
+
+    BankAccount *ptr ;
+    SavingsBankAccount *pointer ;
+    Client(string n , string id , string phone , string address) ;
+    void set_ptr(  BankAccount *) ;
+    void set_pointer( SavingsBankAccount *) ;
     BankAccount* get_ptr() const ;
     string get_name();
+    string get_address();
+    string get_phone();
+    int get_type() const;
 
 };
 
