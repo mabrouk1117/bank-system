1109069bd4de8c54b80d3732748bc1af3596a4e0
AbdEl-Rahman-Fawzy <102333002+AbdEl-Rahman-Fawzy@users.noreply.github.com>
2022-11-15 15:04:18 +0200

    Update Client.cpp

--- Client.cpp
+++ Client.cpp
@@ -2,19 +2,25 @@
 
 #include <utility>
 
-Client::Client(string n , string id , string phone , string address )
+Client::Client(string n , string id , string phone , string address ):ID(std::move(id))
 {
     name = std::move(n) ;
-    ID = std::move(id) ;
+    //ID = std::move(id) ;
     this ->phone = std::move(phone) ;
     this ->address = std::move(address) ;
 
     ptr = nullptr ;
+    pointer = nullptr ;
 }
 
 
 void Client::set_ptr( BankAccount *a) {
+    type = 0;
     this->ptr = a;
+}
+void Client::set_pointer(SavingsBankAccount *a) {
+    type = 1;
+    this->pointer = a;
 }
 
 
@@ -25,3 +31,17 @@ string Client::get_name() {
 BankAccount* Client::get_ptr() const {
     return ptr ;
 }
+
+string Client::get_phone() {
+    return this->phone;
+}
+
+string Client::get_address() {
+    return this->address;
+}
+
+int Client::get_type() const {
+    return type;
+}
+
+
